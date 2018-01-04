ef239e38abed7c9a48bc2627f172d496f164fa58
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-01-04 14:36:13 -0600

    set up modules to provide an array of arrays with all of our data

---
+++ javascripts/formatter.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// format the data
+
+module.exports.formatData = (data) => {
+    console.log('our data', data);
+    //Untangle the array o arrays and make the data ready to present to the user
+};
