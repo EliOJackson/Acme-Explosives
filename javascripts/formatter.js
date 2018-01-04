5313ebbad34800fcc875c2f42a36447be3679c87
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-01-04 15:11:51 -0600

    added in loop to added properties to object property

--- javascripts/formatter.js
+++ javascripts/formatter.js
@@ -5,4 +5,20 @@
 module.exports.formatData = (data) => {
     console.log('our data', data);
     //Untangle the array o arrays and make the data ready to present to the user
+
+    //mutate the products array:
+    // add category key to each item
+    // change "type" val from integer to string value, ie "fragmentation"
+
+   
+    let revisedProds = data[2].map( (prod) => {
+        for(let prop in prod) {
+            prod[prop].category = "new thing"; 
+            prod[prop].type = "blowy up thing"; //data[1] [prod[prop].type]
+
+        }
+        return prod;
+    });
+    console.log('revised', revisedProds);
+
 };
